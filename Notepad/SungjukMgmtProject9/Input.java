39e8f37ff8208b3341ed336080965f38d047f6d3
highjune <highjune37@gmail.com>
2020-07-03 02:49:43 +0900

    update sungjukmgmt9

---
+++ Notepad/SungjukMgmtProject9/Input.java
@@ -0,0 +1,13 @@
+import java.io.ObjectInputStream;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Input {
+	private Scanner scan;
+	private ObjectInputStream ois;
+	
+	public Input(Vector<Student> vector) {
+		this.scan = new Scanner(System.in);
+		this.ois = 
+	}
+}
