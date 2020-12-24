cf0eac4aca9c0b35e34e9d0cadbb1aa60993ae5c
Highjune <57219160+Highjune@users.noreply.github.com>
2020-12-24 18:53:46 +0900

    Update Insert.java

--- CodePractice/AddresslistMgmtProgram2(g)/Insert.java
+++ CodePractice/AddresslistMgmtProgram2(g)/Insert.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
  
 public class Insert {
 	private  ArrayList<Person> list;
-	private Scanner scan;
+	private Scanner scan; 
 	
 	public Insert(ArrayList<Person> list, Scanner scan) {
 		this.list = list;
